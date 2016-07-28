9a0daefea8d7c54b04849b9ed8d826c7d26fd728
tai <tai@korestate.com>
2016-07-28 13:39:50 +0900

    began rebuilding repo

---
+++ heybo-core/__init__.py
@@ -0,0 +1,1 @@
+__author__ == "Tai Kersten"
--- load_program.sh
+++ load_program.sh
@@ -13,6 +13,6 @@ sudo amixer -D default sset PCM 100%
 
 #Execute
 
-cd /home/pi/kobo_stable/ && git pull && python3 cap_and_send.py
+cd /home/pi/heybo/ && git pull && python3 heybo-core/cap_and_send.py
 
 exit 0
